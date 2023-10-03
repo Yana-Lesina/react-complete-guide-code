a82ed799a681033734df7881a50c7e6df888013c
Yana-Lesina <60145733+Yana-Lesina@users.noreply.github.com>
2023-10-03 10:19:27 +0000

    Exmpl: errs while NOT setting keys when rendering statefull components dinamically

--- code/02-understanding-keys/src/components/Expenses/ExpenseItem.js
+++ code/02-understanding-keys/src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,18 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [count, setCount] = React.useState(0);
+
+  const handelClick = () => setCount((prev) => prev + 1);
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${count}</div>
+        <button onClick={handelClick}>Increase</button>
       </div>
     </Card>
   );
